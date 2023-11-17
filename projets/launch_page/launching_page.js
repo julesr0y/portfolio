17b74703ee55652535fa6c060d4cff1d74fb8b81
Jules ROY <julesroydev@gmail.com>
2023-11-18 00:57:08 +0100

    started new site
    launching page

--- projets/launch_page/launching_page.js
+++
@@ -1,8 +0,0 @@
-let menucomplet = document.getElementById('menucomplet');
-let menu = document.getElementById('menu');
-let btn = document.getElementById('but');
-
-btn.addEventListener('click',function(){
-    menu.classList.toggle('apparaitre');
-    menu.style.background = "black"
-});
